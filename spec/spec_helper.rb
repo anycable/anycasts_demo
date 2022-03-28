9034713b7144d644abd061d30559dc31cc03c41a
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-03-28 17:21:51 +0300

    fix(specs): persistence path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,6 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.example_status_persistence_file_path = "tmp/rspec_examples.txt"
   config.order = :random
 end
