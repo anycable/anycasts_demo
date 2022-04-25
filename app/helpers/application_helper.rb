6f1f89376b37d84b43ba554d80fb2fe47044ca6c
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-04-25 13:47:08 +0300

    feat(turbo): add ability to pass channel params for turbo streams

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  # Override stream from to use an enhanced element
+  def turbo_stream_from(*streamables, **attributes)
+    attributes[:channel] = attributes[:channel]&.to_s || "Turbo::StreamsChannel"
+    attributes[:"signed-stream-name"] = Turbo::StreamsChannel.signed_stream_name(streamables)
+    attributes[:params] = attributes[:params]&.to_json
+
+    tag.turbo_cable_stream_source_ext(**attributes)
+  end
 end
---
+++ app/javascript/elements/cable_stream_source.js
@@ -0,0 +1,41 @@
+import { Turbo, cable } from "@hotwired/turbo-rails";
+
+const { connectStreamSource, disconnectStreamSource } = Turbo;
+const { subscribeTo } = cable;
+
+class StreamSourceElement extends HTMLElement {
+  async connectedCallback() {
+    connectStreamSource(this);
+    this.subscription = await subscribeTo(this.channel, {
+      received: this.dispatchMessageEvent.bind(this),
+    });
+  }
+
+  disconnectedCallback() {
+    disconnectStreamSource(this);
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+
+  dispatchMessageEvent(data) {
+    console.log("DATA", data);
+    const event = new MessageEvent("message", { data });
+    return this.dispatchEvent(event);
+  }
+
+  get channel() {
+    const channel = this.getAttribute("channel");
+    const signed_stream_name = this.getAttribute("signed-stream-name");
+
+    let params = {};
+
+    const paramsJSON = this.getAttribute("params");
+
+    if (paramsJSON) {
+      params = JSON.parse(paramsJSON);
+    }
+
+    return { ...params, channel, signed_stream_name };
+  }
+}
+
+customElements.define("turbo-cable-stream-source-ext", StreamSourceElement);
