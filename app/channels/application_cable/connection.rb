4307e40c4501fb8220aa28c77944699dd53f421b
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-07-14 21:16:10 -0400

    feat: support JWT auth when using Action Cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :user
+
+    def connect
+      token = request.params[:jid]
+
+      identifiers = AnyCable::Rails::JWT.decode(token)
+      identifiers.each do |k, v|
+        public_send("#{k}=", v)
+      end
+    rescue JWT::DecodeError
+      reject_unauthorized_connection
+    end
+
+    def handle_open
+      super
+    rescue JWT::ExpiredSignature
+      logger.error "An expired JWT token was rejected"
+      close(reason: "token_expired", reconnect: false) if websocket.alive?
+    end
   end
 end
