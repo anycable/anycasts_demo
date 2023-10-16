dc21f052ebc1655a9e98e4ac1ee270d13d4765b1
Vladimir Dementyev <palkan@hey.com>
2023-10-15 22:58:36 -0700

    Upgrade AnyCable

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,10 +4,10 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@anycable/web", to: "https://ga.jspm.io/npm:@anycable/web@0.7.0/index.js", preload: true
-pin "@anycable/core", to: "https://ga.jspm.io/npm:@anycable/core@0.7.0/index.js", preload: true
+pin "@anycable/core", to: "https://ga.jspm.io/npm:@anycable/core@0.7.9/index.js", preload: true
 pin "nanoevents", to: "https://ga.jspm.io/npm:nanoevents@6.0.2/index.js", preload: true
 pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.1.0/dist/turbo.es2017-esm.js", preload: true
-pin "@anycable/turbo-stream", to: "https://ga.jspm.io/npm:@anycable/turbo-stream@0.3.2/index.js", preload: true
+pin "@anycable/turbo-stream", to: "https://ga.jspm.io/npm:@anycable/turbo-stream@0.4.0/index.js", preload: true
 
 pin "cable", to: "cable.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
