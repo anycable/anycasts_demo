c5a4cfc5c6b7d393ca43e6ea85a0904ce7fce9c5
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-02-06 12:44:49 -0800

    Update seeds and login UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,49 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Channel.insert_all([{name: "general"}, {name: "random"}])
+ruby_channel = Channel.create!(name: "ruby")
+random_channel = Channel.create!(name: "random")
+rails_channel = Channel.create!(name: "rubyonrails")
 
-Channel.pluck(:id) => [id_1, id_2]
-
+# some random users
 User.create!(username: "vova89", password: "qwerty")
 User.create!(username: "alice", password: "qwerty")
+User.create!(username: "bob", password: "qwerty")
+
+# user for direct channels
 jack = User.create!(username: "jack", password: "qwerty")
 sally = User.create!(username: "sally", password: "qwerty")
+bart = User.create!(username: "bart", password: "qwerty")
+homer = User.create!(username: "homer", password: "qwerty")
 
 user_ids = User.pluck(:id)
 
 Message.insert_all(
   [
-    {content: "When will Ruby 3.1 come out?", user_id: user_ids.sample, channel_id: id_1},
-    {content: "Who knows :shrug:", user_id: user_ids.sample, channel_id: id_1},
-    {content: "Rails 7 :heart:", user_id: user_ids.sample, channel_id: id_2},
-    {content: "I'm still on 4.2 :(", user_id: user_ids.sample, channel_id: id_2}
+    {content: "When is Ruby 4 coming?", user_id: user_ids.sample, channel_id: ruby_channel.id},
+    {content: "Who knows :shrug:", user_id: user_ids.sample, channel_id: ruby_channel.id},
+    {content: "If only Ruby 4 had method overloading...", user_id: user_ids.sample, channel_id: ruby_channel.id},
+    {content: "And a proper pipe operator!", user_id: user_ids.sample, channel_id: ruby_channel.id},
+    {content: "Rails 8 :heart:", user_id: user_ids.sample, channel_id: rails_channel.id},
+    {content: "I'm still on 4.2 :(", user_id: user_ids.sample, channel_id: rails_channel.id},
+    {content: "Random thought: pizza is just an open-faced sandwich", user_id: user_ids.sample, channel_id: random_channel.id},
+    {content: "Mind = blown :exploding_head:", user_id: user_ids.sample, channel_id: random_channel.id},
+    {content: "That's the most random thing I've read today", user_id: user_ids.sample, channel_id: random_channel.id}
   ]
 )
 
-direct_channel = Channel.find_or_create_direct_for(jack, sally)
+direct_channel_1 = Channel.find_or_create_direct_for(jack, sally)
 Message.insert_all(
   [
-    {content: "Hey, how is it going?", user_id: jack.id, channel_id: direct_channel.id},
-    {content: "I'm fine, just can't feel my hand...", user_id: sally.id, channel_id: direct_channel.id}
+    {content: "Hey, how is it going?", user_id: jack.id, channel_id: direct_channel_1.id},
+    {content: "I'm fine, just can't feel my hand...", user_id: sally.id, channel_id: direct_channel_1.id}
+  ]
+)
+
+direct_channel_2 = Channel.find_or_create_direct_for(bart, homer)
+Message.insert_all(
+  [
+    {content: "Eat my shorts!", user_id: bart.id, channel_id: direct_channel_2.id},
+    {content: "D'oh!", user_id: homer.id, channel_id: direct_channel_2.id}
   ]
 )
